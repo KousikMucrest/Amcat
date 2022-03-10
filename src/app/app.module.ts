5edb86632adbeafff2f4834b0d1e1cf2ba29a784
Kousik Gupta <kousik.g@mucrest.com>
2022-03-10 11:39:52 +0530

    Initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionsComponent } from './questions/questions.component';
+
+
+import { FusionChartsModule } from 'angular-fusioncharts';
+
+// Load FusionCharts
+import * as FusionCharts from 'fusioncharts';
+// Load PowerCharts module
+import * as Powercharts from 'fusioncharts/fusioncharts.powercharts';
+// Load fusion theme
+import * as FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+
+// Add dependencies to FusionChartsModule
+FusionChartsModule.fcRoot(FusionCharts, Powercharts, FusionTheme);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FusionChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
